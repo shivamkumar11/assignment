a8a62790240a82c1c910704fb5d8124f38271767
shivamkumar11 <shivam991168@gmail.com>
2025-05-26 14:03:25 +0530

    Assignment task end successful....

--- app/src/main/java/com/example/assignment/ProfileActivity.java
+++ app/src/main/java/com/example/assignment/ProfileActivity.java
@@ -1,6 +1,5 @@
 package com.example.assignment;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -15,12 +14,10 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_profile);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(ContextCompat.getColor(this, R.color.my_status_bar_color));
-        }
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.my_status_bar_color));
 
     }
 }
