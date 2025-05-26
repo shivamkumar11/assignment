6cb502f25100241e13d79c039ee6fca04eb90a7d
shivamkumar11 <shivam991168@gmail.com>
2025-05-26 13:46:43 +0530

    Assignment task end successful..

--- app/src/main/java/com/example/assignment/ProfileActivity.java
+++ app/src/main/java/com/example/assignment/ProfileActivity.java
@@ -2,26 +2,19 @@ package com.example.assignment;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.TextureView;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
